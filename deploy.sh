964a32d01c541e7b595afd2cc14ad0c779042cb4
npofsi <npofsi@outlook.com>
2019-04-24 23:04:59 +0800

    config deployment

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+cd ./MCBEServerVersions
+git config --global push.default matching
+git config --global user.email "${GithubEmail}"
+git config --global user.name "${GithubUser}" # 利用在环境变量中定义的信息配置 Git
+git add --all .
+git commit -m "refactor:Auto Tracker of MCBE-Server" # commit 信息
+git push --quiet --force https://${GithubKey}@github.com/${GithubUser}/${GithubRepo}.git master # 部署到指定 Repo 的 master 分支。
